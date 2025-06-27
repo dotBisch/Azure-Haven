31e081c815cb132970613d6c18fc743f2885b7c8
YasminMomo <145175082+YasminMomo@users.noreply.github.com>
2025-06-28 02:25:26 +0800

    Minor changes in home page

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -2,6 +2,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Azure Haven</title>
     <link rel="stylesheet" href="home.css">
+    <link rel="icon" href="{{ asset('Admin/assets/AzureHaven_Logo_2.png') }}" type="image/png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
